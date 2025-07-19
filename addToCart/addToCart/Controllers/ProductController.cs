17cf23d9a7dc2b87da16bc553b81d6a331b7c3bb
usamaa1 <usamariz1997@gmail.com>
2025-07-19 13:27:34 +0500

    till foreign key product add done

---
+++ addToCart/addToCart/Controllers/ProductController.cs
@@ -0,0 +1,92 @@
+﻿using addToCart.Data;
+using addToCart.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace addToCart.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly CartDbContext _context;
+
+        public ProductController(CartDbContext context)
+        {
+            _context = context;
+        }
+
+
+
+        [HttpPost("category")]
+        public IActionResult addCategory(Category category)
+        {
+
+
+            try
+            {
+                if (category != null) { 
+                
+                _context.Categories.Add(category);
+                _context.SaveChanges();
+                return Ok("Category Added");
+                
+                }
+                return StatusCode(204);
+            }
+            catch (Exception ex) { 
+            return BadRequest(ex.Message);
+            
+            }
+
+
+        }
+
+
+
+        [HttpPost]
+        public IActionResult addProduct(Product product)
+        {
+            try
+            {
+
+                if(product != null)
+                {
+                    _context.Products.Add(product);
+                    _context.SaveChanges();
+                    return Ok("Product Added");
+                }
+                return StatusCode(204);
+
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return BadRequest(ex.Message);
+            }
+        }
+
+
+        [HttpGet("catProd")]
+        public IActionResult getProductsWithCategory()
+        {
+            var data = _context.Products.Include(item => item.Category).ToList();
+            
+            
+            
+            return Ok(data);
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ addToCart/addToCart/Data/CartDbContext.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using addToCart.Models;
+
+namespace addToCart.Data;
+
+public partial class CartDbContext : DbContext
+{
+    public CartDbContext()
+    {
+    }
+
+    public CartDbContext(DbContextOptions<CartDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Category> Categories { get; set; }
+
+    public virtual DbSet<Product> Products { get; set; }
+
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Category>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Category__3214EC07B01EA4B3");
+
+            entity.ToTable("Category");
+
+            entity.Property(e => e.CategoryName).HasColumnName("categoryName");
+        });
+
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Product__3214EC0730B9413F");
+
+            entity.ToTable("Product");
+
+            entity.Property(e => e.CategoryId).HasColumnName("categoryId");
+            entity.Property(e => e.ProdDesc).HasColumnName("prodDesc");
+            entity.Property(e => e.ProdImage)
+                .HasDefaultValue("No Image")
+                .HasColumnName("prodImage");
+            entity.Property(e => e.ProdName).HasColumnName("prodName");
+            entity.Property(e => e.ProdPrice).HasColumnName("prodPrice");
+
+            entity.HasOne(d => d.Category).WithMany(p => p.Products)
+                .HasForeignKey(d => d.CategoryId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__Product__categor__3A81B327");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
